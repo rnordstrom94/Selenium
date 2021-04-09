1591d1330c53fb0d1ded7ff75e3e912618c42f1b
rnordstrom94 <rikard.nordstrom@unicus.com>
2021-04-09 09:18:24 +0200

    Initial commit

---
+++ SeleniumPlay/SeleniumPlay/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeleniumPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
