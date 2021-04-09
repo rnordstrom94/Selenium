dde2234256e1f7aa65a338556c050d3337f8a2c6
rnordstrom94 <rikard.nordstrom@unicus.com>
2021-04-09 10:44:17 +0200

    Implement and test task 1
    "Grab page title and place title text in answer slot #1"

---
+++ SeleniumPlay/SeleniumPlay/PageObjects/PlaygroundPage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumPlay.PageObjects
+{
+    public class PlaygroundPage
+    {
+        private IWebDriver _driver;
+        private const string URL = @"http://timvroom.com/selenium/playground/";
+        public static string Title { get; } = "Selenium Playground";
+
+        public PlaygroundPage(IWebDriver driver)
+        {
+            _driver = driver;
+
+            _driver.Navigate().GoToUrl(URL);
+
+            if (driver.Title != Title)
+            {
+                throw new WebDriverException("Page could not be loaded.");
+            }
+        }
+
+        public string FillTitleAnswerSlot()
+        {
+            string title = _driver.Title;
+            IWebElement answerSlot = _driver.FindElement(By.Id("answer1"));
+
+            answerSlot.Clear();
+            answerSlot.SendKeys(title);
+
+            return answerSlot.GetAttribute("value");
+        }
+    }
+}
---
+++ SeleniumPlay/SeleniumPlay/Tests/PlaygroundTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using SeleniumPlay.PageObjects;
+
+namespace SeleniumPlay.Tests
+{
+    [TestFixture]
+    class PlaygroundTest
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new FirefoxDriver();
+        }
+
+        [Test]
+        public void TestPlaygroundPage()
+        {
+            PlaygroundPage pp = new PlaygroundPage(driver);
+
+            Assert.AreEqual(PlaygroundPage.Title, pp.FillTitleAnswerSlot());
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Quit();
+        }
+    }
+}
